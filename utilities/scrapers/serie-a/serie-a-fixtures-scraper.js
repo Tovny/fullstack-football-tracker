b9fe2f513bdfefff7eaa08e63548156cef90e0de
Anton Drofenik <a93.dro@gmai.com>
2021-01-10 13:04:18 +0100

    Added some error checking

--- utilities/scrapers/serie-a/serie-a-fixtures-scraper.js
+++ utilities/scrapers/serie-a/serie-a-fixtures-scraper.js
@@ -13,116 +13,130 @@ const scrapeSerieAFixtures = async () => {
       );
       const html = await res.text();
 
-      const $ = cheerio.load(html);
+      if (res.status == 200 && html) {
+        const $ = cheerio.load(html);
 
-      $(".box-partita").each((j, match) => {
-        const fixture = createFixtureObject();
+        $(".box-partita").each((j, match) => {
+          const fixture = createFixtureObject();
 
-        fixture.league.name = "Italian Serie A";
-        fixture.league.logo = $("#squadre_header .img-responsive").attr("src");
+          fixture.league.name = "Italian Serie A";
+          fixture.league.logo = $("#squadre_header .img-responsive").attr(
+            "src"
+          );
 
-        const info = fixture.info;
+          const info = fixture.info;
 
-        $(match).find(".link-matchreport a:nth-child(3)").attr("href")
-          ? (info.url =
-              "http://www.legaseriea.it" +
-              $(match).find(".link-matchreport a:nth-child(3)").attr("href"))
-          : delete info.url;
+          $(match).find(".link-matchreport a:nth-child(3)").attr("href")
+            ? (info.url =
+                "http://www.legaseriea.it" +
+                $(match).find(".link-matchreport a:nth-child(3)").attr("href"))
+            : delete info.url;
 
-        info.matchday = $(".risultati h3")
-          .text()
-          .replace(/\s\s+/g, " ")
-          .trim()
-          .split("-")[1]
-          .trim();
+          const matchday = $(".risultati h3").text();
+          if (matchday)
+            info.matchday = matchday
+              .replace(/\s\s+/g, " ")
+              .trim()
+              .split("-")[1]
+              .trim();
 
-        fixture.teams.home.name = $(match)
-          .find(".risultatosx h4")
-          .text()
-          .replace(/\s\s+/g, " ")
-          .trim()
-          .toLowerCase();
-        fixture.teams.home.crest =
-          "http://www.legaseriea.it" +
-          $(match).find(".risultatosx img").attr("src");
+          const homeTeam = $(match).find(".risultatosx h4").text();
+          if (homeTeam)
+            fixture.teams.home.name = homeTeam
+              .replace(/\s\s+/g, " ")
+              .trim()
+              .toLowerCase();
 
-        fixture.teams.away.name = $(match)
-          .find(".risultatodx h4")
-          .text()
-          .replace(/\s\s+/g, " ")
-          .trim()
-          .toLowerCase();
-        fixture.teams.away.crest =
-          "http://www.legaseriea.it" +
-          $(match).find(".risultatodx img").attr("src");
+          fixture.teams.home.crest =
+            "http://www.legaseriea.it" +
+            $(match).find(".risultatosx img").attr("src");
 
-        (() => {
-          // Parse the date data
+          const awayTeam = $(match).find(".risultatodx h4").text();
+          if (awayTeam)
+            fixture.teams.away.name = awayTeam
+              .replace(/\s\s+/g, " ")
+              .trim()
+              .toLowerCase();
 
-          let [date, kickOff] = $(match)
-            .find(".datipartita span:nth-child(1)")
-            .text()
-            .split(" ");
+          fixture.teams.away.crest =
+            "http://www.legaseriea.it" +
+            $(match).find(".risultatodx img").attr("src");
 
-          if (!kickOff) kickOff = "20:45";
+          (() => {
+            // Parse the date data
 
-          let [day, month, year] = date.split("/");
-          month = parseInt(month) - 1;
-          let [hour, minute] = kickOff.split(":");
+            let [date, kickOff] = $(match)
+              .find(".datipartita span:nth-child(1)")
+              .text()
+              .split(" ");
 
-          date = new Date(year, month, day, hour, minute);
+            if (!kickOff) kickOff = "20:45";
 
-          if (date.toString() == "Invalid Date") {
-            info.date = "TBC";
-            info.kickOff = "TBC";
-          } else {
-            [date, kickOff] = date.toISOString().split("T");
-            info.date = date;
-            info.kickOff = kickOff.slice(0, 5);
+            let [day, month, year] = date.split("/");
+            month = parseInt(month) - 1;
+            let [hour, minute] = kickOff.split(":");
+
+            date = new Date(year, month, day, hour, minute);
+
+            if (date.toString() == "Invalid Date") {
+              info.date = "TBC";
+              info.kickOff = "TBC";
+            } else {
+              [date, kickOff] = date.toISOString().split("T");
+              info.date = date;
+              info.kickOff = kickOff.slice(0, 5);
+            }
+
+            // Parse the stadium and referee data
+
+            let text = $(match)
+              .find(".datipartita")
+              .text()
+              .replace(/\s\s+/g, "\n")
+              .split("\n");
+            text.forEach((para) => {
+              if (para.includes("Stadium")) {
+                let stadium = para.split(/stadium:/i)[1];
+
+                if (stadium) {
+                  stadium = stadium.trim().toLowerCase();
+                  info.stadium = stadium;
+                }
+              }
+              if (para.includes("Referee")) {
+                let referee = para.split(/referee:/i)[1];
+
+                if (referee) {
+                  referee = referee.trim().toLowerCase();
+                  info.referee = referee;
+                }
+              }
+            });
+          })();
+
+          if ($(match).find(".risultatosx span").text()) {
+            const result = fixture.result;
+
+            const homeScore = parseInt(
+              $(match).find(".risultatosx span").text()
+            );
+            const awayScore = parseInt(
+              $(match).find(".risultatodx span").text()
+            );
+
+            result.home.score = homeScore;
+            result.away.score = awayScore;
           }
 
-          // Parse the stadium and referee data
+          info.matchday = i;
 
-          let text = $(match)
-            .find(".datipartita")
-            .text()
-            .replace(/\s\s+/g, "\n")
-            .split("\n");
-          text.forEach((para) => {
-            if (para.includes("Stadium")) {
-              let stadium = para
-                .split(/stadium:/i)[1]
-                .trim()
-                .toLowerCase();
-              info.stadium = stadium;
-            }
-            if (para.includes("Referee")) {
-              let referee = para
-                .split(/referee:/i)[1]
-                .trim()
-                .toLowerCase();
-              info.referee = referee;
-            }
-          });
-        })();
+          info.status = "Scheduled";
 
-        if ($(match).find(".risultatosx span").text()) {
-          const result = fixture.result;
-
-          const homeScore = parseInt($(match).find(".risultatosx span").text());
-          const awayScore = parseInt($(match).find(".risultatodx span").text());
-
-          result.home.score = homeScore;
-          result.away.score = awayScore;
-        }
-
-        info.matchday = i;
-
-        info.status = "Scheduled";
-
-        fixtures.push(fixture);
-      });
+          fixtures.push(fixture);
+        });
+      }
     }
+
     return fixtures;
   } catch (err) {
     throw new Error(err);
