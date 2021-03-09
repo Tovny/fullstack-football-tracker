67e8d859ea251bead611c9b4f319cd762f5468fe
Anton Drofenik <a93.dro@gmai.com>
2021-03-09 19:56:28 +0100

    Daily commit

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const { mongoURI, PORT } = require("./config");
     app.use("/api/leagues", leagueRoutes);
 
     if (process.env.NODE_ENV === "production") {
-      app.use(express.statis("client/build"));
+      app.use(express.static("client/build"));
 
       app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
