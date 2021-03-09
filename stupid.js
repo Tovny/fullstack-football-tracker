6dcc5bfd5a0f5ee35d7514b37696d985163a49e4
Anton Drofenik <a93.dro@gmai.com>
2021-03-09 13:31:51 +0100

    Daily commit

--- stupid.js
+++
@@ -1,4 +0,0 @@
-const createTable = require("./utilities/create-table");
-const { EPLMatch, SerieAMatch } = require("./models/Match");
-
-createTable(SerieAMatch, "gd").then((res) => console.log(res));
